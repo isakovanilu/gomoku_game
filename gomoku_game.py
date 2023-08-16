76fda5ba20f04e7280841b3e8d9f1d887ccaca94
Nilu Isakova <niluisakova@gmail.com>
2023-08-16 10:52:43 -0500

    added matrix function

--- gomoku_game.py
+++ gomoku_game.py
@@ -2,3 +2,18 @@
 # arbitrary matrix sizes
 # 1st version - H vs H
 
+def update_matrix(matrix, row, column, value):
+    row_len = len(matrix) # row length
+    col_len = len(matrix[0]) # column lenght
+    if (row >= row_len or column >= col_len) or matrix[row][column] != ' ': # if the coordinates greater than matrix size return the function
+        print('Please enter different coordinates')
+    else:
+        for i in range(row_len): # loop throught rows
+            for j in range(col_len): # loop throught columns
+                if i == row and j == column: # if row and columns values match for the given coordinates
+                    matrix[i][j] = value # assign the new values for those coordinates
+    matrix_length = len(matrix)*4 - 3 # to ajust the horizontal line
+    for row in matrix:
+        print(" | ".join(row))
+        print("-" * matrix_length )
+    return matrix
