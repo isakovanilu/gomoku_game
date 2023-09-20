9a29563b3eaf3c9ef850471e3fefc5160d93fab4
Nilu Isakova <niluisakova@gmail.com>
2023-09-20 11:02:18 -0500

    added main module

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from view import GomokuView
+from model import GomokuModel
+from controller import GomokuController
+
+if __name__ == "__main__":
+    print('test')
+    size = int(input("Enter board size: "))
+    model = GomokuModel(size)
+    view = GomokuView()
+    controller = GomokuController(model, view)
+    controller.play_game()
