0117355e098ec1d3f4d7c7a78052025e62afef5a
Nilu Isakova <niluisakova@gmail.com>
2023-10-31 22:15:49 -0500

    use check_winning_move in smart move function

--- controller.py
+++ controller.py
@@ -15,26 +15,28 @@ class SmartComputerPlayer:
         row = random.randint(0, self.board_size - 1)
         col = random.randint(0, self.board_size - 1)
         return row, col
-    
-    def check_winning_move(self, row, col):
-        def check_direction(direction):
-            count = 0
-            for i in range(-4, 5):
-                r, c = row + i * direction[0], col + i * direction[1]
-                if 0 <= r < self.board_size and 0 <= c < self.board_size:
-                    if self.board[r][c] == self.board[row][col]:
-                        count += 1
-                        if count == 5:
-                            return True
-                    else:
-                        count = 0
+  
+    def check_direction(self, row, col, direction):
+        count = 0
+        for i in range(-4, 5):
+            r, c = row + i * direction[0], col + i * direction[1]
+            if 0 <= r < self.board_size and 0 <= c < self.board_size:
+                if self.board[r][c] == self.board[row][col]:
+                    count += 1
+                    if count == 5:
+                        return True
                 else:
                     count = 0
-            return False
+            else:
+                count = 0
+        return False
+         
+    def check_winning_move(self, row, col):
+
         directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
 
         for direction in directions:
-            if check_direction(direction):
+            if self.check_direction(row, col, direction):
                 return True
 
         return False
