83605078e77200d11ed00773a2380eda2dd0a11e
Nilu Isakova <niluisakova@gmail.com>
2023-11-03 11:26:33 -0500

    added smart player

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-
 from view import GomokuView
 from model import GomokuModel
-from controller import GomokuController, DumbComputerPlayer
+from controller import GomokuController, SmartComputerPlayer, DumbComputerPlayer
 
 if __name__ == "__main__":
     board_size = int(input("Enter board size: "))
+    while True:
+        player_type = input("Choose computer player type (smart/dumb): ").strip().lower()
+        if player_type in ["smart", "dumb"]:
+            break
+        else:
+            print("Invalid input. Please choose 'smart' or 'dumb'.")
+    
     model = GomokuModel(board_size)
     view = GomokuView()
-    dumbcomputer = DumbComputerPlayer(board_size)
-    controller = GomokuController(model, view, dumbcomputer)
+    
+    if player_type == "smart":
+        computer_player = SmartComputerPlayer(model.board, board_size)
+    else:
+        computer_player = DumbComputerPlayer(board_size)
+    
+    controller = GomokuController(model, view, computer_player)
     controller.play_game()
