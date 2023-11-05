500383321af20437201f683a950ed5983ed7bb63
Nilu Isakova <niluisakova@gmail.com>
2023-11-05 09:57:22 -0600

    removed dump computer

--- controller.py
+++ controller.py
@@ -40,14 +40,6 @@ class SmartComputerPlayer:
                 return True
 
         return False
-            
-class DumbComputerPlayer:
-    def __init__(self, board_size):
-        self.board_size = board_size
-
-    def make_random_move(self):
-        return random.randint(0, self.board_size-1), random.randint(0, self.board_size-1)
-
 class GomokuController:
     def __init__(self, model, view, dumb_computer=None):
         self.model = model
