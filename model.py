0602a63067ce6ccc13e8cb890fe1267cd2c91da7
Nilu Isakova <niluisakova@gmail.com>
2023-11-24 22:07:03 -0600

    fix variable names

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 import numpy as np
 class GomokuModel:
-    def __init__(self, size):
-        if size < 5:
-            raise ValueError('board size cannot be less than 5')
-        self.size = size
-        self.board = [[' ' for _ in range(size)] for _ in range(size)]
+    def __init__(self, board_size):
+        if board_size < 5:
+            raise ValueError('board board_size cannot be less than 5')
+        self.board_size = board_size
+        self.board = [[' ' for _ in range(board_size)] for _ in range(board_size)]
         self.current_player = "X"
 
     def is_valid_move(self, row, column):
-        return 0 <= row < self.size and 0 <= column < self.size and self.board[row][column] == ' '
+        return 0 <= row < self.board_size and 0 <= column < self.board_size and self.board[row][column] == ' '
 
     def update_board(self, row, column):
         if self.is_valid_move(row, column):
@@ -17,14 +17,14 @@ class GomokuModel:
         return False
     
     def check_winner(self, value):
-        for i in range(self.size):
-            for j in range(self.size):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
                 if self.board[i][j] == value:
                     for dx, dy in [(0, 1), (1, 0), (1, 1), (1, -1)]:
                         win = True
                         for k in range(5):
                             r, c = i + k * dx, j + k * dy
-                            if not (0 <= r < self.size and 0 <= c < self.size and self.board[r][c] == value):
+                            if not (0 <= r < self.board_size and 0 <= c < self.board_size and self.board[r][c] == value):
                                 win = False
                                 break
                         if win:
