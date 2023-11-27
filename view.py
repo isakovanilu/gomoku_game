2a903b6e94f230472eb9e69d797a8c0bcf86e6a6
Nilu Isakova <niluisakova@gmail.com>
2023-11-27 15:43:32 -0600

    Update view.py

--- view.py
+++ view.py
@@ -1,11 +1,11 @@
 class GomokuView:
-    def display_board(self, board):
+    def display_board(self, board) -> None:
         matrix_length = len(board) * 4 - 3
         for row in board:
             print(" | ".join(row))
             print("-" * matrix_length)
 
-    def display_message(self, msg):
+    def display_message(self, msg) -> str:
         print(msg)
 
     def get_input(self, prompt):
