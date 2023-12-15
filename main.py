29d26b4ce5232346f1f859ca245fa130d1438a81
Nilu Isakova <niluisakova@gmail.com>
2023-12-14 23:35:59 -0600

    clean up

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     board_size = int(input("Enter board size: "))
     model = GomokuModel(board_size)
     view = GomokuView()
-    computer_player = SmartComputerPlayer(model.board, board_size)    
+    computer_player = SmartComputerPlayer(model.board, board_size)
     controller = GomokuController(model, view, computer_player)
     controller.play_game()
